1e7c2f9cdf6815e0df5d31331e74f254d3028729
Antonio Alves <antonio.jpsalves@gmail.com>
2023-12-27 15:42:11 -0300

    feat: add flatlist and emptylist components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,21 +1,36 @@
 import { useState } from 'react'
+import { FlatList } from 'react-native'
 
 import { Header } from '@components/Header'
 import { HighLight } from '@components/HighLight'
 import { GroupCard } from '@components/GroupCard'
 import { Container } from './styles'
+import { ListEmpty } from '@components/ListEmpty'
 
 export function Groups() {
 
-  const [groups, setGroups] = useState([])
+  const [groups, setGroups] = useState<string[]>([])
 
 
   return (
     <Container>
       <Header />
       <HighLight title='Turmas' subTitle='Jogue com a sua turma' />
-      <GroupCard title='Teste viado' />
-      <GroupCard title='Teste viado' />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={
+          groups.length === 0 && { flex: 1 }
+        }
+        ListEmptyComponent={<ListEmpty message='Que tal cadastrar a primeira turma?' />}
+      />
+
     </Container>
   )
 }
