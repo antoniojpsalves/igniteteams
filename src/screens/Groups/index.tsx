170db127212c000b4ff20a3902f0de8d117a37a1
Antonio Alves <antonio.jpsalves@gmail.com>
2023-12-27 14:45:30 -0300

    feat: groupcard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react'
+
 import { Header } from '@components/Header'
-import { Container } from './styles'
 import { HighLight } from '@components/HighLight'
+import { GroupCard } from '@components/GroupCard'
+import { Container } from './styles'
 
 export function Groups() {
+
+  const [groups, setGroups] = useState([])
+
+
   return (
     <Container>
       <Header />
       <HighLight title='Turmas' subTitle='Jogue com a sua turma' />
+      <GroupCard title='Teste viado' />
+      <GroupCard title='Teste viado' />
     </Container>
   )
 }
