8addd006c0ff67db276658c33c0ce3317c846492
Antonio Alves <antonio.jpsalves@gmail.com>
2023-12-28 08:55:36 -0300

    feat: initializing stack navigation

--- App.tsx
+++ App.tsx
@@ -5,10 +5,8 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import theme from './src/theme'
 
-import { Groups } from '@screens/Groups'
-import { NewGroup } from '@screens/NewGroup'
 import { Loading } from '@components/Loading'
-import { Players } from '@screens/Players'
+import { Routes } from '@routes/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -20,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   )
 }
