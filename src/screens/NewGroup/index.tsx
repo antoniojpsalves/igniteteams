6e05461e61ca3ed1ad041983a4e7d077ea103c63
Antonio Alves <antonio.jpsalves@gmail.com>
2023-12-27 21:53:38 -0300

    feat: input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from '@components/Header'
 import { Container, Content, Icon } from './styles'
 import { HighLight } from '@components/HighLight'
 import { Button } from '@components/Button'
+import { Input } from '@components/Input'
 
 
 export function NewGroup() {
@@ -14,7 +15,8 @@ export function NewGroup() {
           title='Nova turma'
           subTitle='crie a turma para adicionar as pessoas'
         />
-        <Button title='Criar' />
+        <Input placeholder='Nome da turma' />
+        <Button title='Criar' style={{ marginTop: 20 }} />
       </Content>
     </Container>
   )
